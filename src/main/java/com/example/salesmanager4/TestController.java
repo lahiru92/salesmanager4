1cd2e3627eaaba7938af1f19ee27b76223560995
LahiruJ <lahiru_jayasinghe@combank.net>
2026-07-22 08:37:53 +0530

    RBAC

--- src/main/java/com/example/salesmanager4/TestController.java
+++ src/main/java/com/example/salesmanager4/TestController.java
@@ -1,7 +1,13 @@
 package com.example.salesmanager4;
 
+import java.util.Collection;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/test")
@@ -11,4 +17,10 @@ public class TestController {
         return "test/toast-test";
     }
 
+    @GetMapping("my-authorities")
+    @ResponseBody
+    public Collection<? extends GrantedAuthority> checkMyAuthorities(Authentication authentication) {
+        // This returns the exact list of authorities loaded from your DB tables
+        return authentication.getAuthorities();
+    }
 }
